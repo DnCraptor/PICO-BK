7afca9fd58fda20af61d0e0a849e33356cb56e45
Mike Vengerovich <mikeveng73@gmail.com>
2024-01-31 02:08:47 +0200

    + wii2

--- src/main.cpp
+++ src/main.cpp
@@ -81,14 +81,9 @@ void inInit(uint gpio) {
 
 #ifdef USE_WII
 static repeating_timer_t Wii_timer;
-static bool Wii_stage = false;
 static bool __not_in_flash_func(Wii_Joystick_Timer_CB)(repeating_timer_t *rt) {
-    if (is_WII_Init) {
-        if (Wii_stage)
-            Wii_decode_joy2();
-        else
-            Wii_decode_joy1();
-        Wii_stage = !Wii_stage;
+    if (is_WII_Init()) {
+        Wii_decode_joy();
     }
     return true;
 }
@@ -308,7 +303,7 @@ int main() {
         #endif
     #ifdef USE_WII
     } else {
-        add_repeating_timer_ms(10, Wii_Joystick_Timer_CB, NULL, &Wii_timer);
+        add_repeating_timer_ms(20, Wii_Joystick_Timer_CB, NULL, &Wii_timer);
     }
     #endif
     DBGM_PRINT(("Before keyboard_init"));
