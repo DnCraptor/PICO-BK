2cfd0e6dab30bba9908540e341dea5ecd05ae2fc
Mike Vengerovich <mikeveng73@gmail.com>
2025-09-22 20:08:55 +0300

    + initial to support USB HID keyboard

--- build.bat
+++
@@ -1,4 +0,0 @@
-"C:\Program Files\Raspberry Pi\Pico SDK v1.5.1\cmake\bin\cmake.EXE" -DCMAKE_BUILD_TYPE:STRING=MinSizeRel -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE "-DCMAKE_C_COMPILER:FILEPATH=C:\Program Files\Raspberry Pi\Pico SDK v1.5.1\gcc-arm-none-eabi\bin\arm-none-eabi-gcc.exe" "-DCMAKE_CXX_COMPILER:FILEPATH=C:\Program Files\Raspberry Pi\Pico SDK v1.5.1\gcc-arm-none-eabi\bin\arm-none-eabi-g++.exe" --no-warn-unused-cli -S . -B ./build -G Ninja
-cd build
-ninja
-cd ..\
